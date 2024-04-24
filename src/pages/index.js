6b1f43b7993fb2c8883fbb273f2c9962a3e26256
Yousif Jamal <yjamal@mantech-inc.com>
2024-04-24 18:03:05 -0400

    COnstruction

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <Navbar/>
         <HeroBanner/>
+        <div className={styles.typewriter}><p>Remainder of Website under construction</p></div>
         <div className={styles.scrolltest}>
 
         </div>
