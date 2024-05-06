4c6c64571bbf13d1b75a65dcf7c45e5809450214
Yousif Jamal <yjamal@mantech-inc.com>
2024-05-06 18:48:16 -0400

    HeroBanner changes

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,7 +4,7 @@ import {createTheme, ThemeProvider} from "@mui/material/styles"
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Image from "next/image";
 import backgroundImage from "/public/CameronCrane.JPG";
-import backgroundImage2 from "/public/UNDPMarket2.png";
+import backgroundImage2 from "/public/CameronCrane2.JPG";
 
 
 const theme = createTheme({
